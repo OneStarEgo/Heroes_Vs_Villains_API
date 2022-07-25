87e1ccd656c51a9e8dc5bded9f25ddabcc46f1a4
Rodney McGee <rodneymcgee73@gmail.com>
2022-07-25 17:38:59 -0500

    Updated URLs Files for supers and super_types Apps + Created First View Function for supers and super_types

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from super_types import serializers
+from .models import Super
+from .serializers import SuperSerializer
+
+
+
+@api_view(['GET'])
+def supers_list(request):
+    supers = Super.objects.all()
+    serializer = SuperSerializer(supers, many=True)
+    return Response(serializer.data)
