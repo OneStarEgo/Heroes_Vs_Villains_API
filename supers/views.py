046e7f0de6d1f199d663caf1709187158f8bdb09
Rodney McGee <rodneymcgee73@gmail.com>
2022-07-26 13:52:35 -0500

    Created GET_by_Id Request in supers Views File + Created PUT Request in supers Views File + Created DELETE Request in supers Views File

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from supers import serializers
@@ -16,3 +17,18 @@ def supers_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def super_details(request, pk):
+    super = get_object_or_404(Super,pk=pk)
+    if request.method == 'GET':
+        serializer =SuperSerializer(super)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
