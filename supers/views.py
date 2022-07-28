e9e220ece8ef3af78d3371a314d5bf0450cee82f
Rodney McGee <rodneymcgee73@gmail.com>
2022-07-28 11:00:25 -0500

    Created Get_All_Heroes & Get_All_Villains Request in Supers Views File + Added New Paths For New Requests

--- supers/views.py
+++ supers/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from super_types.models import SuperType
+from super_types.serializers import SuperTypeSerializer
 from supers import serializers
 from .models import Super
 from .serializers import SuperSerializer
@@ -9,9 +11,17 @@ from rest_framework import status
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
-        supers = Super.objects.all()
-        serializer = SuperSerializer(supers, many=True)
+        super_type_name = request.query_params.get('type')
+        print(super_type_name)
+
+        queryset = Super.objects.all()
+
+        if super_type_name:
+            queryset = queryset.filter(super_type__type=super_type_name)
+
+        serializer = SuperSerializer(queryset, many=True)
         return Response(serializer.data)
+
     elif request.method =='POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -32,3 +42,26 @@ def super_details(request, pk):
     elif request.method == 'DELETE':
         super.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET'])
+def super_heroes(request):
+    super_type_name = request.query_params.get('type')
+    queryset = Super.objects.all()
+
+    if super_type_name:
+        queryset = queryset.filter(super_type__type=super_type_name)
+
+    serializer = SuperSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def super_villains(request):
+    super_type_name = request.query_params.get('type')
+    queryset = Super.objects.all()
+
+    if super_type_name:
+        queryset = queryset.filter(super_type__type=super_type_name)
+
+    serializer = SuperSerializer(queryset, many=True)
+    return Response(serializer.data)
