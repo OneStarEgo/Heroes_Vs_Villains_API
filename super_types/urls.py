29285697a197919fd7db4ba67c26369ad507bb53
Rodney McGee <rodneymcgee73@gmail.com>
2022-07-28 17:38:36 -0500

    Added CRUD Functionality to super_types App

--- super_types/urls.py
+++ super_types/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.super_types_list),
+    path('<int:pk>/', views.super_type_details),
 ]
--- super_types/views.py
+++ super_types/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from .models import SuperType
 from .serializers import SuperTypeSerializer
@@ -16,3 +17,18 @@ def super_types_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def super_type_details(request, pk):
+    super_type = get_object_or_404(SuperType,pk=pk)
+    if request.method == 'GET':
+        serializer =SuperTypeSerializer(super_type)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SuperTypeSerializer(super_type, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super_type.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
